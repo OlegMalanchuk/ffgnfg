8f69f4cc578fb7bf0fc806318d1d0907b1b39e40
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-30 10:27:09 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+
+
+//        for (int i = 100; i <= 1000; i += 150) {
+//            // виводимо поточне значення i
+//            System.out.println(i);
+//        }
+//        int i = 100;
+//        while (i<=1000){
+//            System.out.println(i);
+//            i = i +150;
+//        }
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i=i+150;
+//        }while (i<=1000);
+
+//        testClass.setA(3);
+//        testClass.setB(4);
+//        testClass.setC(5);
+//
+//        System.out.println(testClass.getA());
+//        System.out.println(testClass.getB());
+//        System.out.println(testClass.getC());
+
+
+
+    }
+}
